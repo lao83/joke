461c24e06d7df08f6ce8ebe4483a2c2760e41578
tong <159107340+lao83@users.noreply.github.com>
2025-02-11 10:32:40 +0800

    Add files via upload

---
+++ 计算器.py
@@ -0,0 +1,15 @@
+def jisuanqi(a,opt,b):
+    if opt=="+":
+        print(a+b)
+    elif opt=="-":
+        print==(a-b)
+    elif opt=="*":
+        print==(a*b)
+    elif opt=="/":
+        print(a/b)
+    else:
+        print("计算")
+
+
+jisuanqi(999,"*",999)
+jisuanqi(999,"-",223)
